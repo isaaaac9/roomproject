0ffcff3e3ac5e0a5e0827a8a19f1a8f4fa507d69
SibsanSuk <60909299+SibsanSuk@users.noreply.github.com>
2021-10-24 11:23:56 +0700

    เพิ่ม comment

--- Assets/Scipt/Npccontrol.cs
+++ Assets/Scipt/Npccontrol.cs
@@ -1,11 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Npccontrol : MonoBehaviour
 {
+    //สำหรับ AI เดินบน Navmesh (by Aj.San)
     NavMeshAgent agent;
+
+    //เป้าหมายให้ AI เดินไป (by Aj.San)
     public GameObject target;
     // Start is called before the first frame update
     void Start()
